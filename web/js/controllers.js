0e9f80a09cb7be9f966e29f88dae0a5d309b503b
sasha361322 <sasha361322@gmail.com>
2016-09-22 14:23:14 +0300

    asd

--- web/js/controllers.js
+++ web/js/controllers.js
@@ -1,8 +1,20 @@
-var clientApp = angular.module('clientApp', []);
+var clientApp = angular.module('clientApp', ['ngRoute', 'ngResource']);
 
-clientApp.controller('ClientList', function ($scope, $http){
-    $http.get('client/clients.json').success(function (data, status, headers, config) {
-        console.log('This is Data:',data,'\n\n This is Status',status,'\n\nThis is Headers',headers,'\n\nThis is Config',config);
-        $scope.clients = data;
-    });
-});
+clientApp.factory('Client',[
+    '$resource', function ($resource) {
+        return $resource('client/:clientId.:format',{
+            clientId: 'clients',
+            format: 'json'
+        });
+    }
+]);
+
+clientApp.controller('ClientList', function ($scope, $http, Client) {
+
+    /*$http.get('client/clients.json').success(function (data, status, headers, config) {
+            console.log('This is Data:', data, '\n\n This is Status', status, '\n\nThis is Headers', headers, '\n\nThis is Config', config);
+            $scope.clients = data;
+        })*/
+         $scope.clients = Client.query();
+    }
+)
